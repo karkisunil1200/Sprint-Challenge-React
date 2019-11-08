d963aba033b917f692dea2585954e6ae76a39c1b
Sunil Karki <karkisunil1200@gmail.com>
2019-11-08 12:04:26 -0600

    Getting the data almost

--- starwars/src/components/Characters/Characters.js
+++ starwars/src/components/Characters/Characters.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const Characters = () => {
+const Characters = props => {
   return (
-    <div>
-      <h2>Hello from Characters</h2>
+    <div className='characters'>
+      <h2 className Heading>
+        Hello from Characters
+      </h2>
+      <div className='person'>
+        {/* <h4> {props.char.name}</h4>
+        <h5> {props.char.classification}</h5>
+        <h6> {props.char.people}</h6> */}
+      </div>
     </div>
   );
 };
--- starwars/src/components/CharactersList/CharacterList.js
+++ starwars/src/components/CharactersList/CharacterList.js
@@ -4,21 +4,30 @@ import axios from 'axios';
 import Characters from '../Characters/Characters';
 
 const Characterlist = () => {
-  const [characters, setCharacters] = useState({});
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     axios
-      .get()
-      .then()
-      .catch();
+      .get('https://corsanywhere.herokuapp.com/https://lambda-swapi.herokuapp.com/api/people/')
+      .then(res => {
+        console.log('This is the data: ' + res.data.results);
+        setCharacters(res.data.results);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }, []);
 
   return (
     <div>
       <h2>Hell from CharacterList</h2>
-      <Characters />
+      {/* {characters.map((character, index) => {
+        return <Characters key={index} char={character} />;
+      })} */}
     </div>
   );
 };
 
 export default Characterlist;
+
+//https://cors-anywhere.herokuapp.com/

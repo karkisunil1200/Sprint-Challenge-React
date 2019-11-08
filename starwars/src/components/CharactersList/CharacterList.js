704376c170ae6fd363e127ba2ead193fae226ad6
Sunil Karki <karkisunil1200@gmail.com>
2019-11-08 12:25:51 -0600

    Data rendering

--- starwars/src/components/CharactersList/CharacterList.js
+++ starwars/src/components/CharactersList/CharacterList.js
@@ -1,29 +1,36 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
+import {list} from '../../data';
 import Characters from '../Characters/Characters';
 
 const Characterlist = () => {
   const [characters, setCharacters] = useState([]);
+  const [charList, setCharList] = useState(list.results);
 
-  useEffect(() => {
-    axios
-      .get('https://corsanywhere.herokuapp.com/https://lambda-swapi.herokuapp.com/api/people/')
-      .then(res => {
-        console.log('This is the data: ' + res.data.results);
-        setCharacters(res.data.results);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }, []);
+  console.log(charList);
+
+  //   useEffect(() => {
+  //     axios
+  //       .get('https://corsanywhere.herokuapp.com/https://lambda-swapi.herokuapp.com/api/people/')
+  //       .then(res => {
+  //         console.log('This is the data: ' + res.data.results);
+  //         setCharacters(res.data.results);
+  //       })
+  //       .catch(err => {
+  //         console.log(err);
+  //       });
+  //   }, []);
 
   return (
     <div>
-      <h2>Hell from CharacterList</h2>
+      <h2>Hello from CharacterList</h2>
       {/* {characters.map((character, index) => {
         return <Characters key={index} char={character} />;
       })} */}
+      {charList.map((char, index) => {
+        return <Characters result={char.results} key={index} />;
+      })}
     </div>
   );
 };
